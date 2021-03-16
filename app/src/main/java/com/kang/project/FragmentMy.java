f3518a247ec82532c5cffe9adb341ea4224a5473
Hyunstagram <fgrt1006@naver.com>
2021-03-16 21:29:50 +0900

    Day 1 - fragment_my 수정

--- app/src/main/java/com/kang/project/FragmentMy.java
+++ app/src/main/java/com/kang/project/FragmentMy.java
@@ -1,17 +1,27 @@
 package com.kang.project;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class FragmentMy  extends Fragment {
 
     View view;
+
+    private Context mContext;
+    private FloatingActionButton fab_main, fab_sub1, fab_sub2;
+    private Animation fab_open, fab_close;
+    private boolean isFabOpen = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
@@ -22,6 +32,32 @@ public class FragmentMy  extends Fragment {
     }
 
     public void init() {
+        mContext = getContext();
+        fab_open = AnimationUtils.loadAnimation(mContext, R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(mContext, R.anim.fab_close);
+        fab_main = (FloatingActionButton) view.findViewById(R.id.fab_main);
+        fab_sub1 = (FloatingActionButton) view.findViewById(R.id.fab_sub1);
+        fab_sub2 = (FloatingActionButton) view.findViewById(R.id.fab_sub2);
 
+        fab_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isFabOpen) {
+                    fab_main.setImageResource(R.drawable.booking);
+                    fab_sub1.startAnimation(fab_close);
+                    fab_sub2.startAnimation(fab_close);
+                    fab_sub1.setClickable(false);
+                    fab_sub2.setClickable(false);
+                    isFabOpen = false;
+                } else {
+                    fab_main.setImageResource(R.drawable.booking);
+                    fab_sub1.startAnimation(fab_open);
+                    fab_sub2.startAnimation(fab_open);
+                    fab_sub1.setClickable(true);
+                    fab_sub2.setClickable(true);
+                    isFabOpen = true;
+                }
+            }
+        });
     }
 }
