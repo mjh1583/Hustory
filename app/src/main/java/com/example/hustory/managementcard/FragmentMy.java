5f31c7c910f1820cef518ebac966c8b63451ba5d
mjh1583 <mjh1583@naver.com>
2021-03-21 12:49:56 +0900

    관리카드 부분 합침

--- app/src/main/java/com/example/hustory/managementcard/FragmentMy.java
+++ app/src/main/java/com/example/hustory/managementcard/FragmentMy.java
@@ -1,11 +1,15 @@
 package com.example.hustory.managementcard;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -15,7 +19,10 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -25,10 +32,24 @@ import com.example.hustory.R;
 import com.example.hustory.login.LoginActivity;
 import com.example.hustory.managementcard.CardActivity;
 import com.example.hustory.managementcard.LetterActivity;
+import com.example.hustory.user.User;
 import com.example.hustory.userInfo.UserInfo;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import static android.app.Activity.RESULT_OK;
 
 public class FragmentMy extends Fragment {
 
@@ -36,6 +57,20 @@ public class FragmentMy extends Fragment {
     Button button_letter;
 
     View view;
+
+    private final int GALLERY_CODE = 10;
+    private ImageView imageview;
+    private FirebaseStorage storage;
+    private FirebaseAuth firebaseAuth;
+
+    private DatabaseReference mDatabase;
+    TextView save;
+    EditText my_school, my_major, my_company_1, my_company_2, my_company_3;
+    private TextView version_student;
+    private TextView delete;
+
+
+    private Uri filePath;
 
     private Context mContext;
     private FloatingActionButton fab_main, fab_sub1, fab_sub2;
@@ -120,6 +155,58 @@ public class FragmentMy extends Fragment {
             auto_editor.apply();
             startLoginActivity();
         });
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        my_school = (EditText) view.findViewById(R.id.my_school);
+        my_major = (EditText) view.findViewById(R.id.my_major);
+        my_company_1 = (EditText) view.findViewById(R.id.my_company_1);
+        my_company_2 = (EditText) view.findViewById(R.id.my_company_2);
+        my_company_3 = (EditText) view.findViewById(R.id.my_company_3);
+        version_student=(TextView)view.findViewById(R.id.version_student);
+        save = (TextView) view.findViewById(R.id.save);
+        button_card = (Button) view.findViewById(R.id.button_card);
+        button_letter = (Button) view.findViewById(R.id.button_letter);
+
+        imageview = (ImageView) view.findViewById(R.id.icon_student);
+
+        delete = (TextView)view.findViewById(R.id.delete);
+
+        imageview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //이미지를 선택
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "이미지를 선택하세요."), 0);
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getUsermy_school = my_school.getText().toString();
+                String getUsermy_major = my_major.getText().toString();
+                String getUsermy_company_1 = my_company_1.getText().toString();
+                String getUsermy_company_2 = my_company_2.getText().toString();
+                String getUsermy_company_3 = my_company_3.getText().toString();
+                String getUserversion_student = version_student.getText().toString();
+                uploadFile();
+
+                //hashmap 만들기
+                HashMap result = new HashMap<>();
+                result.put("my_school", getUsermy_school);
+                result.put("my_major", getUsermy_major);
+                result.put("my_company_1", getUsermy_company_1);
+                result.put("my_company_2", getUsermy_company_2);
+                result.put("my_company_3", getUsermy_company_3);
+                result.put("ersion_student", getUserversion_student);
+
+
+                writeNewUser(UserInfo.UID, getUsermy_school, getUsermy_major, getUsermy_company_1, getUsermy_company_2, getUsermy_company_3,getUserversion_student);
+
+            }
+        });
     } //init()
 
     public void startLoginActivity () {
@@ -142,6 +229,93 @@ public class FragmentMy extends Fragment {
             }
         }
         return view.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        //request코드가 0이고 OK를 선택했고 data에 뭔가가 들어 있다면
+        if(requestCode == 0 && resultCode == RESULT_OK){
+            filePath = data.getData();
+
+            try {
+                //Uri 파일을 Bitmap으로 만들어서 ImageView에 집어 넣는다.
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), filePath);
+                imageview.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+    //upload the file
+    private void uploadFile() {
+        //업로드할 파일이 있으면 수행
+        if (filePath != null) {
+            //업로드 진행 Dialog 보이기
+            final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setTitle("업로드중...");
+            progressDialog.show();
+
+            //storage
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+
+
+            String filename = UserInfo.UID;
+            //storage 주소와 폴더 파일명을 지정해 준다.
+            StorageReference storageRef = storage.getReferenceFromUrl("gs://hustory-82cc1.appspot.com").child("images/" + filename);
+            //올라가거라...
+            storageRef.putFile(filePath)
+                    //성공시
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss(); //업로드 진행 Dialog 상자 닫기
+                            Toast.makeText(getActivity().getApplicationContext(), "업로드 완료!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    //실패시
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getActivity().getApplicationContext(), "업로드 실패!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    //진행중
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            @SuppressWarnings("VisibleForTests") //이걸 넣어 줘야 아랫줄에 에러가 사라진다. 넌 누구냐?
+                                    double progress = (100 * taskSnapshot.getBytesTransferred()) /  taskSnapshot.getTotalByteCount();
+                            //dialog에 진행률을 퍼센트로 출력해 준다
+                            progressDialog.setMessage("Uploaded " + ((int) progress) + "% ...");
+                        }
+                    });
+        } else {
+            Toast.makeText(getActivity().getApplicationContext(), "파일을 먼저 선택하세요.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void writeNewUser(String userId, String my_school, String my_major, String my_company_1, String my_company_2, String my_company_3,String version_student) {
+        User user = new User(my_school, my_major, my_company_1, my_company_2, my_company_3,version_student);
+
+        mDatabase.child("Member").child(userId).child("management").child("1").setValue(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Write was successful!
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Write failed
+
+                    }
+                });
     }
 }
 
--- app/src/main/java/com/example/hustory/managementcard/LetterActivity.java
+++ app/src/main/java/com/example/hustory/managementcard/LetterActivity.java
@@ -2,15 +2,36 @@ package com.example.hustory.managementcard;
 
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.hustory.R;
+import com.example.hustory.user.User;
+import com.example.hustory.user.User3;
+import com.example.hustory.userInfo.UserInfo;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class LetterActivity extends AppCompatActivity {
+
+    EditText background,character,ict_experience,motivation;
+    Button button_modify1;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +39,45 @@ public class LetterActivity extends AppCompatActivity {
         setContentView(R.layout.layout_letter);
 
         init();
+
+        background = findViewById(R.id.background);
+        character = findViewById(R.id.character);
+        ict_experience = findViewById(R.id.ict_experience);
+        motivation = findViewById(R.id.motivation);
+        button_modify1= findViewById(R.id.button_modify);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        background = (EditText)findViewById(R.id.background);
+        character = (EditText)findViewById(R.id.character);
+        ict_experience = (EditText)findViewById(R.id.ict_experience);
+        motivation = (EditText)findViewById(R.id.motivation);
+        button_modify1= findViewById(R.id.button_modify);
+
+        readUser();
+
+        button_modify1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getUserbackground = background.getText().toString();
+                String getUsercharacter = character.getText().toString();
+                String getUserict_experience = ict_experience.getText().toString();
+                String getUsermotivation = motivation.getText().toString();
+
+
+
+
+                //hashmap 만들기
+                HashMap result = new HashMap<>();
+                result.put("background", getUserbackground);
+                result.put("character", getUsercharacter);
+                result.put("ict_experience", getUserict_experience);
+                result.put("motivation", getUsermotivation);
+
+
+                writeNewUser(UserInfo.UID,getUserbackground,getUsercharacter,getUserict_experience,getUsermotivation);
+
+            }
+        });
     }
 
     @Override
@@ -35,6 +95,49 @@ public class LetterActivity extends AppCompatActivity {
             }
         }
         return super.dispatchTouchEvent(ev);
+    }
+
+    private void writeNewUser(String userId, String background, String character , String ict_experience,String motivation) {
+        User3 user = new User3(background,character,ict_experience,motivation);
+
+        mDatabase.child("Member").child(userId).child("management").child("3").setValue(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Write was successful!
+                        Toast.makeText(LetterActivity.this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Write failed
+                        Toast.makeText(LetterActivity.this, "저장을 실패했습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private void readUser(){
+        mDatabase.child("Member").child("management").child("3").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                if(dataSnapshot.getValue(User.class) != null){
+                    User post = dataSnapshot.getValue(User.class);
+                    Log.w("FireBaseData", "getData" + post.toString());
+                } else {
+                    Toast.makeText(LetterActivity.this, "데이터 없음...", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public void init() {
